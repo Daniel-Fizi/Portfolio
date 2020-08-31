ce9f5e5a99b8b17a62ad6ab3eda923a4160ca307
Daniel-Fizi <47127276+Daniel-Fizi@users.noreply.github.com>
2020-08-31 15:39:43 +0200

    Updates

---
+++ precache-manifest.3c986dc7f776fa987546f27c1eeeefcd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db448daaf612fe92fc7ef0323df77489",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff0db694e06c518447ce",
+    "url": "/static/css/main.d22d4e21.chunk.css"
+  },
+  {
+    "revision": "d8849d9c8a56708ec5f7",
+    "url": "/static/js/2.290cd6ab.chunk.js"
+  },
+  {
+    "revision": "89ea5e0ed13930f7b9c9c2f392e27cfe",
+    "url": "/static/js/2.290cd6ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff0db694e06c518447ce",
+    "url": "/static/js/main.30dd18be.chunk.js"
+  },
+  {
+    "revision": "c222bdc77c92db122a75",
+    "url": "/static/js/runtime-main.1bd5d8ce.js"
+  },
+  {
+    "revision": "292eaa82a2798ae69768a23ef80000d3",
+    "url": "/static/media/myAvatar.292eaa82.svg"
+  }
+]);
